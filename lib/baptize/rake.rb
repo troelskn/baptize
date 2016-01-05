64c45b9c9e7edf9db98faf9abc981f0faefffb9b
Troels Knak-Nielsen <troelskn@gmail.com>
2016-01-05 10:56:33 +0100

    Dropped namespace for tasks

--- lib/baptize/rake.rb
+++ lib/baptize/rake.rb
@@ -1,21 +1,18 @@
-Rake.application.in_namespace('baptize') do
+Rake::Task.define_task(:list) do
+  puts "Available packages"
+  Baptize::Registry.packages.values.each do |package|
+    puts [package.name, "\t", package.description].join
+  end
+end
 
-  Rake::Task.define_task(:list) do
-    Baptize::Registry.packages.values.each do |package|
-      puts [package.name, "\t", package.description].join
+Rake::Task.define_task(:apply) do
+  Baptize::Registry.policies.keys.each do |role|
+    on roles(role), in: :parallel do |host|
+      Baptize::Registry.apply_policy role, host, self
     end
   end
-
-  Rake::Task.define_task(:apply) do
-    Baptize::Registry.policies.keys.each do |role|
-      on roles(role), in: :parallel do |host|
-        Baptize::Registry.apply_policy role, host, self
-      end
-    end
-  end
-
 end
 
 Rake::Task.define_task(:default) do
-  Rake::Task['baptize:list'].invoke
+  Rake::Task['list'].invoke
 end
